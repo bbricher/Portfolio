b245f257281f08a1162360b2c793e303432e121f
Briana Bricher <bbricher@udallas.edu>
2020-01-23 14:45:41 -0600

    added links for sites

--- client/src/Components/Projects/Projects.js
+++ client/src/Components/Projects/Projects.js
@@ -4,6 +4,8 @@ import theOffice from './theoffice.gif';
 import strangerThings from './strangerthings.gif';
 import lyricalConnection from './lyrical-connection.gif';
 import bamazon from './manager-example.gif';
+import githubImg from './iconfinder_github_circle_gray_107139.png';
+import wwwImg from './iconfinder_22-Website_3213298.png';
 import Slide from 'react-reveal/Slide';
 import Fade from 'react-reveal/Fade';
 import { basename } from "path";
@@ -19,7 +21,14 @@ class Projects extends Component {
                 <Slide bottom>
                     <header>
                         <h4>
-                            The Office Guessing Game
+                            The Office Guessing Game <a>   </a> 
+                            <a href="https://bbricher.github.io/Word-Guess-Game/">  
+                                <Image src={wwwImg} style={{width: "25px"}}  roundedCircle />
+                            </a> 
+                            <a>   </a> 
+                            <a href="https://github.com/bbricher/Word-Guess-Game">  
+                                <Image src={githubImg} style={{width: "25px"}}  roundedCircle />
+                            </a> 
                         </h4>
                         <br/>
                         <Image src={theOffice} style={{width: "800px"}} />
@@ -47,7 +56,14 @@ class Projects extends Component {
                 <Fade bottom big>
                     <header>
                         <h4>
-                            Stranger Things Trivia
+                            Stranger Things Trivia <a>   </a> 
+                            <a href="https://bbricher.github.io/TriviaGame/">  
+                                <Image src={wwwImg} style={{width: "25px"}}  roundedCircle />
+                            </a> 
+                            <a>   </a>
+                            <a href="https://github.com/bbricher/TriviaGame">  
+                                <Image src={githubImg} style={{width: "25px"}}  roundedCircle />
+                            </a> 
                         </h4>
                         <br/>
                         <Image src={strangerThings} style={{width: "800px"}} />
@@ -101,7 +117,14 @@ class Projects extends Component {
                 <Fade bottom big>
                     <header>
                         <h4>
-                            Lyrical Connection
+                            Lyrical Connection <a>   </a> 
+                            <a href="https://om289560.github.io/project-1/">  
+                                <Image src={wwwImg} style={{width: "25px"}}  roundedCircle />
+                            </a> 
+                            <a>   </a>
+                            <a href="https://github.com/om289560/project-1">  
+                                <Image src={githubImg} style={{width: "25px"}}  roundedCircle />
+                            </a> 
                         </h4>
                         <br/>
                         <Image src={lyricalConnection} style={{width: "800px"}} />
@@ -155,7 +178,10 @@ class Projects extends Component {
                 <Fade bottom big>
                     <header>
                         <h4>
-                            "Bamazon" Node Site [view Github here: ]
+                            "Bamazon" Node Site <a>   </a> 
+                            <a href="https://github.com/bbricher/storefront">  
+                                <Image src={githubImg} style={{width: "25px"}}  roundedCircle />
+                            </a> 
                         </h4>
                         <br/>
                         <Image src={bamazon} style={{width: "800px"}} />
@@ -209,7 +235,7 @@ class Projects extends Component {
                 <Fade bottom big>
                     <header>
                         <h4>
-                            "Bamazon" Node Site [view Github here: ]
+                            
                         </h4>
                         <br/>
                         <Image src={bamazon} style={{width: "800px"}} />
