b39299924b69272d47b30c2fb01a7cc6f48baf2c
Briana Bricher <bbricher@udallas.edu>
2020-01-23 14:57:28 -0600

    added additional route to server

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "./client/public/index.html"));
+});
+
 // Add routes, both API and view
 app.use(routes);
 
