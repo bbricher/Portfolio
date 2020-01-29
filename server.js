54950d4b79e266b5ceb2ac4be0703fcf30dbfee1
Briana Bricher <bbricher@udallas.edu>
2020-01-29 07:45:58 -0600

    test of build

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "./client/public/index.html"));
-});
 
 // Add routes, both API and view
 app.use(routes);
