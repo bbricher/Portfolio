c49ccaf2e49266643d617f29fb2a4613847a2d3c
Briana Bricher <bbricher@udallas.edu>
2020-01-29 07:51:10 -0600

    took out mongo

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/contactPortfolio");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/contactPortfolio");
 
 // Start the API server
 app.listen(PORT, function() {
